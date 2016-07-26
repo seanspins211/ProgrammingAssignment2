c1455375aa1d78310a282d0a761934f386c931ad
seanspins211 <sbearden@ucsd.edu>
2016-07-26 12:26:26 -0700

    Stage 1: copy example funtions with appropriate change.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #assume matrix is always invertible.
+        #m <-solve(x)
+        
+        m<-NULL
+        set <- function(y){
+                x<<-y
+                m<<-NULL
+        } 
+        
+        get <- function() x
+        setInverse <- function(solve) m<<-solve
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse, 
+             getInverse = getInverse)
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will determine if matrix x has a computed inverse. If true, then it returns
+## that Inverse. If false, then that inverse will be computed and returned.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getInverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
